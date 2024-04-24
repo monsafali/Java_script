297b221a148271bfbc988ef0820d1cfafa75003c
monsafali <monsafali272@gmail.com>
2024-04-24 06:55:17 +0500

    array practise task

---
+++ 02_arrays/README.md
@@ -0,0 +1,37 @@
+Q1:for the given start state of an array change it to final form using methods
+
+start = ['january','july','march','august']
+find = ['july','june','march','august']
+
+
+
+Q2:Return the index of the "javascript" from the given array if it was reserved.
+
+['c','c++','html','javascript','python','java','c#','sql']
+
+
+Qs3. Write a JavaScript program to get the first n elements of an array. [n can be any
+positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [7, 9, 0]
+
+Q4:
+Write a JavaScript program to get the last n elements of an array. [n can be any
+positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [9, 0, -2]
+
+
+Qs5
+. Write a JavaScript program to check whether a string is blank or not.
+
+
+Q6:
+Write a JavaScript program to test whether the character at the given (character)
+index is lower case.
+
+Q7:
+Write a JavaScript program to strip leading and trailing spaces from a string.
+
+Q8
+Write a JavaScript program to check if an element exists in an array or not.
---
+++ 02_arrays/app.js
@@ -0,0 +1,72 @@
+//Question NO 1 for the given start state of an array change it to final form using methods
+
+let start = ['january','july','march','august']
+
+// start.shift()
+// start.shift()
+// start.unshift('june')
+// start.unshift('july')
+// let final = start
+// console.log(final)
+
+// using splice methods
+// start.splice(0,2,'july','june')
+// console.log(start)
+
+
+
+// Q2:
+let Arr = ['c','c++','html','javascript','python','java','c#','sql']
+// console.log(Arr.reverse().indexOf('javascript'))
+
+
+// Q3:
+
+// let number = [7, 9, 0, -2,7, 9, 0, -2]
+// let n = 3;
+
+// let ans = number.slice(0,3)
+// console.log(ans);
+
+
+// Q4:
+
+// let ans = number.slice(number.length-n);
+// console.log(ans)
+
+
+// Q5
+// let str1 = prompt('Enter your string');
+// if(str1.length == 0){
+//     console.log('string is empty');
+// }else{console.log('string is not empty')};
+
+
+// Q6:
+// let str = prompt('enter you string');
+// let idx = 3;
+// if (str[idx] == str[idx].toLowerCase()){
+//     console.log('character is lowercase')
+// } else{
+//     console.log('character is not lowercase')
+// }
+
+
+// Q7
+// let str3 = '    this iss randome string         '
+// console.log(str3.trim());
+
+
+
+// Q8
+
+
+let array = ['hello','how','are','you'];
+let Name = 'love';
+if(array.lastIndexOf(Name)!= -1){
+    console.log('element exist in the array')
+}else{
+    console.log("eleemtnd does't exist in array")
+}
+
+
