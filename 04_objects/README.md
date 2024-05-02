0e98f793855186e33a2f5c5482d3379143397bea
monsafali <monsafali272@gmail.com>
2024-05-02 20:42:12 +0500

    object question and guessing game

---
+++ 04_objects/README.md
@@ -0,0 +1,15 @@
+Create a program that generates a random number representing a dice roll.
+[The number should be between 1 and 6].
+
+
+
+Qs2. Create an object representing a car that stores the following properties for the
+car: name, model, color.
+Print the car’s name.
+
+
+
+Qs3. Create an object Person with their name, age and city.
+Edit their city’s original value to change it to “New York”.
+Add a new property country and set it to the United States.
+
---
+++ 04_objects/app.js
@@ -0,0 +1,61 @@
+// Q1
+// let random = Math.floor(Math.random()*5)+1;
+// console.log(random)
+
+
+
+// Q2
+
+// let cars ={
+//     car_name: "honda",
+//     Model: 1992,
+//     color: "red",
+//     Ingin_number: 14587,
+//     Chesiz_num : "PBTD08" 
+// }
+// console.log(cars.car_name)
+
+
+
+// Q3
+
+let Person = {
+    Name: "monsaf Ali",
+    Age: 25,
+    City: "Fort Abbas",
+}
+
+Person.country = "Pakistan",
+console.log(Person);
+
+
+
+
+
+
+
+
+
+
+
+// const max = prompt('Enter Maximum Number');
+
+// const random = Math.floor(Math.random()*max) +1;
+
+// let guess = prompt('guess the numbeer');
+
+
+// while(true){
+//     if(guess == "quit"){
+//         console.log('user quit')
+//         break;
+//     }
+//     if (guess == random){
+//         console.log('you are right congratas random number was:',random)
+//         break;
+//     } else if (guess < random){
+//         guess = prompt('your guess was too small please try again');
+//     } else {
+//         guess = prompt('you guess is too large please try again')
+//     }
+// }
